f118ce390229b067872603cb648a9e1335d164ce
rvrocks212 <110858730+rvrocks212@users.noreply.github.com>
2024-07-02 18:39:06 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,16 +1,36 @@
 document.onreadystatechange = function() {
-    if (document.readyState !== 'complete') {
-        document.querySelector("body").style.overflow = "hidden";
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector("#loader").style.visibility = "visible";
-    } else {
-        setTimeout(function() {
-            document.querySelector("#loader").style.display = "none";
-            document.querySelector("body").style.overflow = "auto";
-            document.querySelector("body").style.visibility = "visible";
-        },7000); 
-    }
+  if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+      if (document.readyState !== 'complete') {
+          document.querySelector("body").style.overflow = "hidden";
+          document.querySelector("body").style.visibility = "hidden";
+          document.querySelector("#loader").style.visibility = "visible";
+      } else {
+          setTimeout(function() {
+              document.querySelector("#loader").style.display = "none";
+              document.querySelector("body").style.overflow = "auto";
+              document.querySelector("body").style.visibility = "visible";
+              
+              gsap.to('.char', {
+                  y: 0,
+                  stagger: 0.05,
+                  duration: .1
+              });
+          }, 7000);
+      }
+  } else {
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector("body").style.overflow = "auto";
+      document.querySelector("body").style.visibility = "visible";
+      
+      gsap.to('.char', {
+          y: 0,
+          stagger: 0.05,
+          duration: .1
+      });
+  }
 };
+
+
 
 const link = document.querySelectorAll('.project');
 const linkHoverReveal = document.querySelectorAll('.hover-reveal');
@@ -31,3 +51,12 @@ for(let i = 0; i < link.length; i++) {
     linkImages[i].style.transform = 'scale(0.8, 0.8)';
   })
 }
+
+const lenis= new Lenis()
+
+function raf(time){
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFramer(raf);
